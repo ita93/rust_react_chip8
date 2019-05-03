d730ea7f3870dcf78b2db115deef7c184a565992
Nguyễn Đình Phi <phind.uet@gmail.com>
2019-05-03 22:30:57 +0800

    Add remaining opcodes
    Adding remaining opcodes.

--- rust/chip8_emu/src/cpu.rs
+++ rust/chip8_emu/src/cpu.rs
@@ -11,6 +11,9 @@
 * - We also need a stack and a stack pointer to save the current address
 *   before we perform a jump, so we can be back to a overpassed addr easily.
 */
+    
+extern crate rand;
+use rand::Rng;
 use keyboard::Keyboard;
 use display::Display;
 use font::FONT_SET;
@@ -85,7 +88,7 @@ impl CPU{
         let x = ((opcode & 0x0F00) >> 8) as usize;
         let y = ((opcode & 0x00F0) >> 4) as usize;
         let kk = ((opcode & 0x00FF)) as u8;
-        let nnn = ((opcode) & 0x0FFF);
+        let nnn = (opcode) & 0x0FFF;
         match (op, x, y, n){
             //Don't need to implement SYS (dead)
             //CLS
@@ -171,6 +174,84 @@ impl CPU{
                 self.registers[0xF] = self.registers[x] & 0x10;
                 self.registers[x] = self.registers[x] << 1;
             },
+            //SNE Vx, Vy
+            (9, _, _, 0) => {
+                self.pc += if self.registers[x] != self.registers[y] {2} else {0}
+            },
+            //LD I, addr
+            (0xA, _, _, _) => {
+                self.ir = nnn;
+            },
+            //JP V0, addr
+            (0xB, _, _, _) => {
+                self.pc += self.registers[0] as u16 + nnn;
+            },
+            //RND Vx, byte 
+            (0xC, _, _, _) => {
+                /*The interpreter generates a random number from 0 to 255,
+                which is then ANDed with the value kk. The results are 
+                stored in Vx. See instruction 8xy2 for more information on AND.*/
+                let mut rng = rand::thread_rng();
+                self.registers[x] = rng.gen::<u8>() & kk;
+            },
+            //DRW Vx, Vy, nibble
+            (0xD, _, _, _) => {
+                let sprites = &self.memory[self.ir as usize..(self.ir + n as u16) as usize];
+                self.registers[0xF] = if self.display.draw(self.registers[x] as usize, self.registers[y] as usize, sprites) {
+                    1
+                } else {
+                    0
+                };
+            },
+            //SKP Vx
+            (0xE, _, 9, 0xE) => {
+                self.pc += if self.keyboard.is_key_down(x as usize) {2} else {0};
+            },
+            //SKNP Vx
+            (0xE, _, 0xA, 1) => {
+                self.pc += if !self.keyboard.is_key_down(x as usize) {2} else {0};
+            },
+            //LD Vx, DT
+            (0xF, _, 0, 7) => {
+                self.registers[x] = self.delay_timer;
+            },
+            //LD Vx, K 
+            (0xF, _, 0, 0xA) => {
+                //Wait till K pressed, then store value key in REG X.
+            },
+            //LD DT, Vx 
+            (0xF, _, 1, 5) => {
+                self.delay_timer = self.registers[x];
+            },
+            //LD ST, Vx
+            (0xF, _, 1, 8) => {
+                //ignore it for now.
+            },
+            //ADD I, Vx
+            (0xF, _, 1, 0xE) => {
+                self.ir += self.registers[x] as u16;
+            },
+            //LD F, Vx 
+            (0xF, _, 2, 9) => {
+                self.ir = self.registers[x] as u16 * 5;
+            },
+            //LD B, Vx
+            (0xF, _, 3, 3) => {
+                self.memory[self.ir as usize] = self.registers[x] / 100;
+                self.memory[self.ir as usize + 1] = (self.registers[x] / 10) % 10;
+                self.memory[self.ir as usize + 2] = (self.registers[x] % 100) % 10;
+            },
+            //LD [I], Vx
+            //[I] means value that stored at addr I.
+            (0xF, _, 5, 5) => {
+                self.memory[self.ir as usize..=(self.ir as usize + x)]
+                    .copy_from_slice(&self.registers[0..=x]);
+            },
+            //LD Vx, [I]
+            (0xF, _, 6, 5) => {
+                self.registers[0..=x].
+                    copy_from_slice(&self.memory[self.ir as usize..=(self.ir as usize + x)]);
+            }
             _ => {
                 //igonre it
             }
